5b643df4ccdb96ea3d21839b11fac8dfd87eddf3
DenisIndenbom <59681620+denisindenbom@users.noreply.github.com>
2021-10-10 19:41:14 +0300

    small changes

--- create_default_json_data.py
+++ create_default_json_data.py
@@ -1,8 +1,11 @@
 import json
 
-data = {
-    'chat_id_example':{'admin_id':[],'user_id':[],'rating':[],'toxic':[],'positive':[],'is_toxic':[],'ban_mode': False}
-}
+if input('Ты правда хочешь пересоздать дату? (y/n)').lower() == 'y':
+    data = {
+        'chat_id_example':{'admin_id':[],'user_id':[],'rating':[],'toxic':[],'positive':[],'is_toxic':[],'ban_mode': False}
+    }
 
-with open('users.json', 'w') as file:
-    file.write(json.dumps(data))
+    with open('users.json', 'w') as file:
+        file.write(json.dumps(data))
+else:
+    print('Операция отменена')
